1610b457d2488284c490e63443b5d5d84472009b
john <johnc@4us.org>
2018-01-31 16:22:19 -0800

    3.1

---
+++ 3.1.py
@@ -0,0 +1,54 @@
+# John Crosbie
+# CIS 122
+# Ch 3.1
+# 02/05/18
+
+
+# 3.1.2
+print("Q2 is:")
+print('C' + chr(35))
+
+# 3.1.4
+print("Q4 is:")
+print(chr(ord('B'))) #the ASCII Value of B is 66
+
+# 3.1.6
+print("Q6 is:")
+list1 = [17, 3, 12, 9, 10]
+list1.sort()
+print("Minimum:", list1[0])
+print("Maximum:", list1[-1])
+
+# 3.1.8
+print("Q8 is:")
+letter = 'D'
+spread = ord('a') - ord('A')
+print(chr(ord(letter) + spread))
+
+# values for 9-20
+a = 2
+b = 3
+
+# 3.1.10
+print("Q10 is:")
+print(((5 - a) * b) < 7)
+
+# 3.1.12
+print("Q12 is:")
+print(a ** b == b ** a)
+
+# 3.1.14
+print("Q14 is:")
+print(3e-2 < .01 *a)
+
+# 3.1.16
+print("Q16 is:")
+print((a * a < b) or not(a * a < a))
+
+# 3.1.18
+print("Q18 is:")
+print(not(a < b) or not (a < (b + a)) )
+
+
+
+

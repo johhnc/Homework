a576826ba4edf6f7f0ece288d2cd8be09036f22f
john <johnc@4us.org>
2018-02-02 09:34:58 -0800

    3.2

--- 3.2.py
+++ 3.2.py
@@ -46,3 +46,42 @@ elif (not(65 <= ord(letter) <= 90)):
     print("You did not enter a letter")
 else:
     print(letter, "is a vowel.")
+
+# 3.2.12
+print("Q12 is:")
+number = 5
+if number <0:
+    print("Negative")
+else:
+    if number == 0:
+     print("zero")
+    else:
+        print("positive")
+
+# 3.2.14
+print("Q14 is:")
+if " ":
+    print("An empty string is true")
+else:
+    print("An empty string is false")
+
+# 3.2.24
+print("Q24 is:")
+feet = eval(input("How tall(in feet) is the Statue of Liberty?  "))
+if (feet <= 141):
+    print("Nope")
+if (feet > 141):
+    if (feet < 161):
+        print("Good")
+    else:
+        print("Nope")
+
+# 3.2.26
+print("Q26 is:")
+item = float(input("Enter the number of bagels: "))
+cost1 = item * .75
+cost2 = item * .60
+if item < 6:
+    print("the cost is:", "$",round(cost1, 3))
+if item >6:
+    print("the cost is:", "$", round(cost2, 3))

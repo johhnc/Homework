2a29605c39316a272edf0cec3b3fd812052997a1
john <johnc@4us.org>
2018-02-07 15:01:12 -0800

    3.4

---
+++ 3.4.py
@@ -0,0 +1,65 @@
+# John Crosbie
+# CIS 122
+# Ch 3.4
+# 02/12/18
+
+# 3.4.2
+print("Q2 is:")
+range(-11, -7)
+
+# 3.4.4
+print("Q4 is:")
+range(2010, 2030, 5)
+
+# 3.4.6
+print("Q6 is:")
+range(1)
+
+# 3.4.8
+print("Q8 is:")
+range(12, 2, -5)
+
+# 3.4.10
+print("Q10 is:")
+print("range(4)")
+
+# 3.4.12
+print("Q12 is:")
+print("range(4, 0, -1)")
+
+# 3.4.14
+print("Q14 is:")
+print("range(7, 8)")
+
+# 3.4.18
+print("Q18 is:")
+for i in range(3 ,2):
+    print(2 * i)
+
+# 3.4.20
+print("Q20 is:")
+for i in range(-9, 0, 3):
+    print(i)
+
+# 3.4.22
+print("Q22 is:")
+n = 3
+total = 0
+for i in range(1, n + 1):
+    total += 1
+print(total)
+
+# 3.4.24
+print("Q24 is:")
+for countdown in range(10, 0, -1):
+    print(countdown)
+
+# 3.4.26
+print("Q26 is:")
+numCaps = 0
+name = "United States of America"
+for ch in name:
+    if ch.isupper():
+        numCaps +=0
+print(numCaps)
+

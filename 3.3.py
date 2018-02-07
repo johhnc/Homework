ceedb58aee6fa093569929680b2cadbca81b5ef7
john <johnc@4us.org>
2018-02-07 14:10:15 -0800

    3..4

--- 3.3.py
+++ 3.3.py
@@ -46,21 +46,44 @@ if (numTries == 7) and (year != 1964):
     print("\nYour 7 tries are up. The answer is 1964")
 
 # 3.3.16
-
+print("Q16 is:")
 restitution = float(input("Enter coefficient of restitution: "))
 height = float(input("Enter initial height in meters: "))
 # meters to centimeters
 height *= 100
-distanceTraveled = 0
+distance = 0
 # first bounce
 bounces = 1
-distanceTraveled = height
+distance = height
 while height * restitution >= 10:
     bounces += 1
 height = restitution * height
-distanceTraveled += 2 * height
-# back to meters
-distanceTraveled /= 100
+distance += 2 * height
+# centimeters to meters
+distance /= 100
 print("Number if bounces:", bounces)
-print("Meters traveled: {0:,.2f}".format(distanceTraveled))
+print("Meters traveled: {0:,.2f}".format(distance))
+
+# 3.3.18
+print("Q18 is:")
+factors = []
+n = int(input("Enter a positive integer (> 1): "))
+f = 2
+while n > 1:
+    if n // f == n / f:
+        factors.append(str(f))
+        n = n // f
+    else:
+        f += 1
+result = " ".join(factors)
+print("Prime factors are", result)
+
+# 3.3.20
+print("Q20 is:")
+startYear = 2011 # as of October 21, 2011
+population = 7 # population is 7 billion
+while population <= 8:
+    population = 1.011 * population
+    startYear += 1
+print("World population will be \n8 billion in the year", str(startYear) + ".")
 

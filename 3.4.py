170cc9b404662188ed959e12dfa9d1e837920c54
John Crosbie <johnc@4us.org>
2018-02-10 21:05:02 -0800

    3.4

--- 3.4.py
+++ 3.4.py
@@ -59,6 +59,74 @@ numCaps = 0
 name = "United States of America"
 for ch in name:
     if ch.isupper():
-        numCaps +=0
+        numCaps += 0
 print(numCaps)
+
+# 3.4.28
+print("Q28 is:")
+word = "cloudier"
+newWord = ""
+evenIndex = True
+for ch in word:
+    if evenIndex:
+        newWord += ch
+    evenIndex = not evenIndex
+print(newWord)
+
+# 3.4.30
+print("Q30 is:")
+for ch in "Python":
+    break
+print(ch)
+
+# 3.4.32
+print("Q32 is:")
+list1 = [2, 9, 6, 7, 13, 3]
+maxOfOdds = 0
+for num in list1:
+    if (num % 2 == 1) and (num > maxOfOdds):
+        maxOfOdds = num
+print(maxOfOdds)
+
+# 3.4.34
+print("Q34 is:")
+numOfNumbers = 0
+list1 = ["three", 4, 5.7, "six", "seven", 8, 3.1416]
+for item in list1:
+    if isinstance(item, str):
+        continue
+    numOfNumbers
+print(numOfNumbers)
+
+# 3.4.36
+print("Q36 is:")
+# I'm looking over a four leaf clover.
+leaves = ("sunshine", "rain", "the roses that bloom in the lane", "somebody I adore")
+number = 1
+for leaf in leaves:
+    print("Leaf", str(number) + ':', leaf)
+    number += 1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 

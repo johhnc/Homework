abdfe3d581b0b71bbb64ee3eb2bd8c7bd1a34a82
John Crosbie <johnc@4us.org>
2018-02-10 23:55:05 -0800

    3.4

--- debug2.py
+++ debug2.py
@@ -1,11 +1,6 @@
-states = ["Delaware", "Pennsylvania", "New Jersey", "Georgia", "Connecticut", "Massachusetts", "Maryland",
-          "South Carolina", "New Hampshire", "Virginia", "New York", "North Carolina", "Rhode Island", "Vermont",
-          "Kentucky", "Tennessee", "Ohio", "Louisiana", "Indiana", "Mississippi", "Illinois", "Alabama", "Maine",
-          "Missouri", "Arkansas", "Michigan", "Florida", "Texas", "Iowa", "Wisconsin", "California", "Minnesota",
-          "Oregon", "Kansas", "West Virginia", "Nevada", "Nebraska", "Colorado", "North Dakota", "South Dakota",
-          "Montana", "Washington", "Idaho", "Wyoming", "Utah", "Oklahoma", "New Mexico", "Arizona", "Alaska", "Hawaii"]
-# 2.4.46
-print("Q46 is: needs work")
-list2 = states[2].split() + states[-4].split()
-list2.remove("New")
-print(list2)
+print("Q56 is:")
+sum = 0
+for i in range(1, 101):
+    sum += i
+print("The sum 1 + 2 + ... + 100")
+print("is", str(sum) + '.')

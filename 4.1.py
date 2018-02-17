1cbd50cda5583d7d78edc34024e01aefb03581ca
John Crosbie <johnc@4us.org>
2018-02-17 06:43:25 -0800

    4.1

--- 4.1.py
+++ 4.1.py
@@ -205,3 +205,25 @@ main()
 
 # 4.1.32
 print("Q32 is:")
+months = []
+def main():
+## display months containing the letter r.
+    global months
+    fillList()
+    months = deleteNoRs()
+    displayMonths()
+def fillList():
+    global months
+    infile = open("Months.txt", 'r')
+    months = [line.rstrip() for line in infile]
+    infile.close
+def deleteNoRs():
+    reducedList = []
+    for i in range(12):
+        if 'r' in months[i].lower():
+          reducedList.append(months[i])
+    return reducedList
+def displayMonths():
+    print("The R months are:")
+    print((", ").join(months))
+main()

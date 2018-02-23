0371f57984210baaf3ba9b3bc9c72ca4e65a7ccd
John Crosbie <johnc@4us.org>
2018-02-22 17:07:13 -0800

    5.1

--- 5.1.py
+++ 5.1.py
@@ -2,3 +2,19 @@
 # CIS 122
 # Ch 5.1
 # 02/19/18
+
+# 5.1.2
+print("Q2 is:")
+outfile = open("Greetings.txt", 'w')
+outfile.write("Hello\n")
+outfile.write("Aloha\n")
+outfile.close()
+infile = open("Greetings.txt", 'r')
+text = infile.readline().rstrip()
+infile.close()
+print(text)
+
+# 5.1.4
+print("Q4 is:")
+list1 = ["Hello", "Aloha\n"]
+

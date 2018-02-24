42ea77c1ea0a708826c72c2f579a6115995e4048
johhnc <johnc@4us.org>
2018-02-24 05:14:39 -0800

    5.2

--- 5.2.py
+++ 5.2.py
@@ -94,4 +94,26 @@ for justice in supremeCourt:
 
 # 5.1.14
 print("Q14 is:")
-
+def main():
+    stateInput = input("Enter a state abbreviation: ")
+    justices = getJusticesByState(stateInput)
+    fixCurrentJustices(justices)
+    justices.sort(key=lambda justice: justice[5] - justice[4], reverse=True)
+    print("\n{0:18} {1:20} {2}".format("Justice", "Appointing Pres", "Yrs Served"))
+    for justice in justices:
+        print("{0:18} {1:20} {2}".format(justice[0] + " " + justice[1],
+                                         justice[2].split(" ")[-1], justice[5] - justice[4]))
+def getJusticesByState(stateInput):
+    infile = open("Justices.txt", 'r')
+    listOfRecords = [line for line in infile if line.split(',')[3] == stateInput]
+    infile.close()
+    for i in range(len(listOfRecords)):
+        listOfRecords[i] = listOfRecords[i].split(',')
+        listOfRecords[i][4] = int(listOfRecords[i][4])
+        listOfRecords[i][5] = int(listOfRecords[i][5])
+        return listOfRecords
+def fixCurrentJustices(justices):
+    for justice in justices:
+        if justice[5] == 0:
+            justice[5] = 2015
+main()

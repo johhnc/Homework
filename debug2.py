55a9d19593ad8af555f94905f5d2546774c7ee29
johhnc <johnc@4us.org>
2018-02-24 23:59:34 -0800

    5.2

--- debug2.py
+++ debug2.py
@@ -1,7 +1,23 @@
-print("Q54 is:")
-largest = eval(input("Enter a number: "))
-for i in range(2):
-    num = eval(input("Enter a number: "))
-    if num > largest:
-        largest = num
-print("Largest number:", largest)
+def main():
+## Display justices from a specified state.
+    state = input("Enter a state abbreviation: ")
+    justices = getJusticesByState(state)
+    fixCurrentJustices(justices)
+    justices.sort(key=lambda justice: justice[5] - justice[4], reverse=True)
+    print("\n{0:18} {1:20} {2}".format("Justice", "Appointing Pres","Yrs Served"))
+    for justice in justices:
+        print("{0:18} {1:20} {2}".format(justice[0] + " " + justice[1], justice[2].split(" ")[-1], justice[5] - justice[4]))
+def getJusticesByState(state):
+    infile = open("Justices.txt", 'r')
+    listOfRecords = [line for line in infile if line.split(',')[3] == state]
+    infile.close()
+    for i in range(len(listOfRecords)):
+        listOfRecords[i] = listOfRecords[i].split(',')
+        listOfRecords[i][4] = int(listOfRecords[i][4])
+        listOfRecords[i][5] = int(listOfRecords[i][5])
+        return listOfRecords
+def fixCurrentJustices(justices):
+    for justice in justices:
+        if justice[5] == 0:
+            justice[5] = 2015
+main()

c71839add0752a008d86b8d919e47b673b0d471b
johhnc <johnc@4us.org>
2018-02-25 13:42:06 -0800

    5.2

--- debug2.py
+++ debug2.py
@@ -1,23 +1,22 @@
 def main():
-## Display justices from a specified state.
     state = input("Enter a state abbreviation: ")
-    justices = getJusticesByState(state)
-    fixCurrentJustices(justices)
-    justices.sort(key=lambda justice: justice[5] - justice[4], reverse=True)
-    print("\n{0:18} {1:20} {2}".format("Justice", "Appointing Pres","Yrs Served"))
-    for justice in justices:
-        print("{0:18} {1:20} {2}".format(justice[0] + " " + justice[1], justice[2].split(" ")[-1], justice[5] - justice[4]))
+    judges = getJusticesByState(state)
+    fixCurrentJudges(judges)
+    judges.sort(key=lambda judge: judge[5] - judge[4], reverse=True)
+    print("\n{0:18} {1:20} {2}".format("Justice", "Appointing Pres", "Yrs Served"))
+    for judge in judges:
+        print("{0:18} {1:20} {2}".format(judge[0] + " " + judge[1], judge[2].split(" ")[-1], judge[5] - judge[4]))
 def getJusticesByState(state):
     infile = open("Justices.txt", 'r')
-    listOfRecords = [line for line in infile if line.split(',')[3] == state]
+    records = [line.rstrip() for line in infile if line.split(',')[3] == state]
     infile.close()
-    for i in range(len(listOfRecords)):
-        listOfRecords[i] = listOfRecords[i].split(',')
-        listOfRecords[i][4] = int(listOfRecords[i][4])
-        listOfRecords[i][5] = int(listOfRecords[i][5])
-        return listOfRecords
-def fixCurrentJustices(justices):
-    for justice in justices:
-        if justice[5] == 0:
-            justice[5] = 2015
+    for i in range(len(records)):
+        records[i] = records[i].split(',')
+        records[i][4] = int(records[i][4])
+        records[i][5] = int(records[i][5])
+        return records
+def fixCurrentJudges(judges):
+    for judge in judges:
+        if judge[5] == 0:
+            judge[5] = 2015
 main()

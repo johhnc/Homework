ebc75b8c4f42d6c1089f887d9c5bb07596413288
johhnc <johnc@4us.org>
2018-03-12 16:08:32 -0700

    FPCommented
    Still has an issue with the "None" being printed out when restarting the enter clue number

--- Final Project/FinalGameCommentedStudentFile.py
+++ Final Project/FinalGameCommentedStudentFile.py
@@ -5,7 +5,8 @@
 # Name: getQuestionNumber
 # Input: string
 # Returns: string
-
+def getQuestionNumber(introMessage):
+    return input(introMessage + "\n")
 # Create a method that accepts the word the
 # user guessed, as well as the correct answer
 # and compares the two. If they match, return a
@@ -16,6 +17,8 @@
 # Name: correctMessage
 # Input: string, string
 # Returns: string
+def correctMessage(userGuess, questionAnswer):
+    print("Congratulations! You entered", userGuess, "and the correct answer is", questionAnswer + "!\n")
 
 # Create a method that accepts the word the
 # user guessed, as well as the correct answer
@@ -24,6 +27,10 @@
 # Name: isCorrect
 # Input: string, string
 # Returns: bool
+def isCorrect(userGuess, questionAnswer):
+    if userGuess.lower() == questionAnswer.lower():
+        return True
+    return False
 
 # Create a method that accepts a string as a message
 # and asks the user a question with that message (input).
@@ -32,7 +39,8 @@
 # Name: tryAnswer
 # Input: string
 # Returns: string
-
+def tryAnswer(message):
+    return input(message)
 # Create a method that accepts the correct answer
 # Return a string that tells the user that the
 # game is over, and what the correct answer was. 
@@ -41,7 +49,8 @@
 # Name: gameOver
 # Input: string
 # Returns: string
-
+def gameOver(questionAnswer):
+    print("Sorry! the word was", questionAnswer + ".", "Please Play Again!\n")
 def main():
     # Create a dictionary of dictionaries that contain the
     # following pairs: An integer key, and a dictionary value
@@ -56,30 +65,37 @@ def main():
 
     # Create a string variable that holds the following message:
     # "Enter as question number from 1 to 5, and you will play that clue. Enter -1 to quit "
-
+    introMessage = "Enter as question number from 1 to 5, and you will play that clue and answer the question" \
+                   ". Enter -1 to quit"
     # Create a bool variable to tell the game whether to keep
     # looping, or to quit the game. Initialize the variable to True
-
+    keepGoing = True
     # Begin a while loop that the rest of the game will live inside
     # The while loop should be a terminal loop that will be ended
     # with a break statement later if the user enters a value of "-1"
-
+    while keepGoing:
         # Using a try/except block, ask the user for a number
         # and convert the result to an integer. Be sure to use
         # the getQuestionNumber method to ask for the number
         # If the user's answer cannot be converted to an integer
         # make sure the exception block ends with a continue statement
         # so that the user is asked the question again
-
+        try:
+            questionNumber = int(getQuestionNumber(introMessage))
+        except:
+            continue
         # If the user entered "-1" as the question number
         # tell the user "Goodbye", and set the variable called 
         # keepGoing to False. Also make sure the block ends
         # with a break statement so the while loop terminates
-
+        if questionNumber == int("-1"):
+            print("Goodbye, thanks for playing!")
+            break
         # If the question number is not a value between
         # 1 and 5, return to the top of the loop with a
         # continue statement so the question is asked again
-
+        if questionNumber not in range(1, 6):
+            continue
         # Set a variable that creates a string message showing the clue
         # and ends with "What is it?"
         questionMessage = list(cluesDictionary[questionNumber].values())[0] + ". What is it?"
@@ -91,7 +107,7 @@ def main():
         # Tell the user the clue, and ask them for the correct answer
         # Be sure to use the tryAnswer method for this, and to pass
         # in the right argument (the variable questionMessage)
-
+        userGuess = tryAnswer(questionMessage + "\n")
         # If the user entered the correct word, congratulate them
         # and tell them what they entered and what the correct word was.
         # Be sure to use the isCorrect and correctMessage methods 
@@ -103,13 +119,19 @@ def main():
         # You must tell them that they are either on the first letter,
         # the last letter or another letter in the middle when you
         # present the letters.
+        if isCorrect(userGuess, questionAnswer):
+            print(correctMessage(userGuess, questionAnswer))
+            continue
+
+        else:
+            print("that was incorrect!")
 
             # Create a counter variable to know the current
             # position you are at in the answer word
-
+            index = 0
             # Begin a for loop to loop through the answer word that
             # was answered incorrectly
-
+            for i in questionAnswer:
                 # If this is the first letter, tell them so,
                 # and what the letter is
                 # Increment the counter
@@ -119,13 +141,23 @@ def main():
                 # is correct. If the new answer is correct
                 # tell them so using the correctMessage method
                 # and break from the for loop
+                if index == 0:
+                    print("The first letter is:", i)
+                    index += 1
+                    userGuess = tryAnswer("Try Again!\n")
+                    if isCorrect(userGuess, questionAnswer):
+                        print(correctMessage(userGuess, questionAnswer))
+                        break
 
                 # If this is the last letter, tell them so,
                 # and what the letter is
                 # Increment the counter
-                # Tell them what the last letter was and 
+                # Tell them what the last letter was and
                 # tell them, using the gameOver method, to play again
- 
+                elif index == len(questionAnswer) - 1:
+                    print("the last letter is:", i)
+                    print(gameOver(questionAnswer))
+                    break
                 # If this is a letter other than the first or last letter,
                 # tell them so, and what the letter is
                 # Increment the counter
@@ -135,8 +167,12 @@ def main():
                 # is correct. If the new answer is correct
                 # tell them so using the correctMessage method
                 # and break from the for loop
-
-            #end for
-    #end while
+                else:
+                    print("The next letter is:", i)
+                    index += 1
+                    userGuess = tryAnswer("Try Again!\n")
+                    if isCorrect(userGuess, questionAnswer):
+                        print(correctMessage(userGuess, questionAnswer))
+                        break
                         
 main()
